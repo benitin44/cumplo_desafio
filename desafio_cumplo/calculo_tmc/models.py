be2e3c4cac2bd4ca0ec6d3a5088e7d5d79e2d79a
Benito Giralt <bgiralt@gmail.com>
2020-09-09 12:56:07 -0300

    readme format

--- desafio_cumplo/calculo_tmc/models.py
+++ desafio_cumplo/calculo_tmc/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class TasaMaximaConvencional(models.Model):
-    titulo = models.CharField(max_length=200)
-    subtitulo = models.CharField(max_length=200)
-    valor = models.FloatField()
-    fecha = models.DateField()
-    hasta = models.DateField()
-    tipo = models.IntegerField()
